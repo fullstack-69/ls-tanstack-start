da05560ede7ab63dcd009695302db32defccc4a1
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-07-21 06:40:22 +0700

    Add UAParser and update server functions to include user-agent parsing

--- src/server_functions.ts
+++ src/server_functions.ts
@@ -1,8 +1,20 @@
 import { createServerFn } from "@tanstack/react-start";
+import { getRequestHeaders } from "@tanstack/react-start/server";
 // "cannot find module or its corresponding type declarations" error. Ignore it for now.
 //@ts-ignore
 import { staticFunctionMiddleware } from "@tanstack/start-static-server-functions";
+import { UAParser } from "ua-parser-js";
 
 export const getNavTime = createServerFn({ method: "GET" })
 	.middleware([staticFunctionMiddleware])
 	.handler(async () => new Date().toLocaleTimeString());
+
+export const getGreeting = createServerFn({ method: "GET" }).handler(
+	async () => {
+		const ua = getRequestHeaders()["user-agent"] ?? "";
+		return {
+			browser: new UAParser(ua).getResult().browser.name,
+			time: new Date().toLocaleTimeString(),
+		};
+	},
+);
